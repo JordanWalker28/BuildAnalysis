bd528b0153aea63207954ef56fefe922659538c1
Jordan Walker <jordanswalker28@gmail.com>
2020-07-20 23:01:29 +0100

    Updated some of the Json reading

--- JsonParser.py
+++ JsonParser.py
@@ -7,7 +7,7 @@ def openFile(file):
     f.close()
     return data
 
-def createDictionary(data):
+def extractInfo(data):
     jsonnn_tree = objectpath.Tree(data['suites'])
 
     classes = []
@@ -15,26 +15,25 @@ def createDictionary(data):
 
     result_case = tuple(jsonnn_tree.execute('$..className'))
     result_duration = tuple(jsonnn_tree.execute('$..duration'))
+    result_name = tuple(jsonnn_tree.execute('$..name'))
 
     classes = list(result_case)
     duration = list(result_duration)
-
+    name = list(result_name)
+    
     duration.remove(duration[0])
-
-    name_value_tuples = zip(classes, duration)
-    name_to_value_dict = {}
-    for key, value in name_value_tuples:
-        if key in name_to_value_dict:
-            pass
-        else:
-            name_to_value_dict[key] = value
-
-    return name_to_value_dict
+    name.remove(name[0])
+    
+    print("result classname: " + str(len(classes)))
+    print("result name: " + str(len(name)))
+    print("result duration: " + str(len(duration)))
+    
+    print(classes[1] + " " + name[1] + " " + str(duration[1]))
 
 
 
 data = openFile('json.json')
-dictionary = createDictionary(data)
+newData = extractInfo(data)
 
-for x, y in dictionary.items():
-    print(x,y)
+#for x, y in dictionary.items():
+    #print(x,y)
